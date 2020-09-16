3ac8298f5534705259f1a71a2350d091ddf384a8
Leo <karta09104@gmail.com>
2020-09-16 16:32:46 +0800

    fix newer usage of BertConfig

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -202,7 +202,7 @@ class BertConfig(object):
     @classmethod
     def from_dict(cls, json_object):
         """Constructs a `BertConfig` from a Python dictionary of parameters."""
-        config = BertConfig(vocab_size_or_config_json_file=-1)
+        config = BertConfig(json_object["vocab_size"])
         for key, value in json_object.items():
             config.__dict__[key] = value
         return config
